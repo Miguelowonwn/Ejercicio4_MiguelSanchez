a0ec6b413f67b9c69e9360ab9d97741332592518
Miguel <miguelalejandros2001@gmail.com>
2023-12-04 21:03:17 +0100

    Mi primer commit

---
+++ Ejercicio4_MiguelSanchez/src/Ejercicio4_MiguelSanchez/Ejercicio4_MiguelSanchez.java
@@ -0,0 +1,7 @@
+package Ejercicio4_MiguelSanchez;
+
+public class Ejercicio4_MiguelSanchez {
+	public static void main(String[] args) {
+		System.out.println("Hola, este es el Ejercicio 4 de mi práctica de Git");
+	}
+}
