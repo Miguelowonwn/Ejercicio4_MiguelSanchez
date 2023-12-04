82ab165c81b62f77207d5a6ea3423b2a1b71e835
Miguel <miguelalejandros2001@gmail.com>
2023-12-04 21:11:08 +0100

    Mi segundo commit

--- Ejercicio4_MiguelSanchez/src/Ejercicio4_MiguelSanchez/Ejercicio4_MiguelSanchez.java
+++ Ejercicio4_MiguelSanchez/src/Ejercicio4_MiguelSanchez/Ejercicio4_MiguelSanchez.java
@@ -2,6 +2,6 @@ package Ejercicio4_MiguelSanchez;
 
 public class Ejercicio4_MiguelSanchez {
 	public static void main(String[] args) {
-		System.out.println("Hola, este es el Ejercicio 4 de mi práctica de Git");
+		System.out.println("Hola, este es el Ejercicio 4");
 	}
 }
